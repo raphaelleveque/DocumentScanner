653f15cc5b6d6effd6d15ff08216c56f6539a563
Raphael Leveque <raphaellev1102@gmail.com>
2022-04-13 19:24:02 -0300

    Reorganizing

--- DocumentScanner/DocumentScanner/main.cpp
+++ DocumentScanner/DocumentScanner/main.cpp
@@ -1,25 +1,41 @@
-
 #include <opencv2/imgcodecs.hpp>
- #include <opencv2/highgui.hpp>
- #include <opencv2/imgproc.hpp>
- #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
- using namespace cv;
- using namespace std;
+Mat preProcessing(Mat originalImg) {
+    Mat grayImg, blurImg, cannyImg;
+    cvtColor(originalImg, grayImg, COLOR_BGR2GRAY);
+    GaussianBlur(grayImg, blurImg, Size(3, 3), 3, 0);
+    Canny(blurImg, cannyImg, 25, 75);
+    
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+    Mat dilateImg;
+    dilate(cannyImg, dilateImg, kernel);
+    return dilateImg;
+}
 
- ///////////////// Webcam //////////////////////
+void getContours() {
+    
+    
+}
 
- int main() {
 
-      VideoCapture cap(0);
-      Mat img;
 
-      while (true) {
-
-      cap.read(img);
-      imshow("Image", img);
-      waitKey(1);
-
-  }
-   return 0;
-  }
+int main() {
+    string path = "Resources/paper.jpg";
+    Mat originalImg = imread(path);
+    
+    //resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5);
+    
+    Mat thresholdImg = preProcessing(originalImg);
+    imshow("Image Dilation", thresholdImg);
+    imshow("Image", originalImg);
+    waitKey(0);
+    
+    
+    
+    return 0;
+}
